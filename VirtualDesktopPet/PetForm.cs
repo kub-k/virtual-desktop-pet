182673d83b4fc77f2e3e3ef66b3e8d43f185f751
kübra <kubra3.y@gmail.com>
2026-04-26 22:10:05 +0300

    create project
    when the program runs:
    - the window will be borderless
    - it will be transparent
    - it will stay on top of the screen
    - it will appear on the taskbar
    there is no pet yet, only a pink square

---
+++ VirtualDesktopPet/PetForm.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VirtualDesktopPet
+{
+    public partial class PetForm : Form
+    {
+        public PetForm()
+        {
+            InitializeComponent();
+            ConfigureForm();
+            InitializeContextMenu();
+            this.Paint += PetForm_Paint;
+        }
+        private void ConfigureForm()
+        {
+            this.Text = "Virtual Desktop Pet";
+
+            FormBorderStyle = FormBorderStyle.None;
+            StartPosition = FormStartPosition.Manual;
+
+            Width = 300;
+            Height = 300;
+
+            TopMost = true;
+            ShowInTaskbar = true;
+
+            BackColor = Color.Lime;
+            TransparencyKey = Color.Lime;
+
+            DoubleBuffered = true;
+
+            Location = new Point(500, 300);
+        }
+        private void InitializeContextMenu()
+        {
+        }
+
+        private void PetForm_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            using (Brush brush = new SolidBrush(Color.LightPink))
+            {
+                int size = 80;
+                int x = (this.ClientSize.Width - size) / 2;
+                int y = (this.ClientSize.Height - size) / 2;
+                g.FillRectangle(brush, x, y, size, size);
+            }
+        }
+
+        private void PetForm_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
